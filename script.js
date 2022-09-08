0b3aaf16daff0112b76831f6b85afd102869a483
Grattade07 <joe.woodcock15@hotmail.co.uk>
2022-09-08 21:00:29 +0100

    Added hover effect to all cells

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let cols = document.getElementsByClassName("cell");
 function makeRows(rowNum) {
     for (r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
+        row.id = "gridRow";
         container.appendChild(row).className = "gridRow";
     };
 }
@@ -19,9 +20,32 @@ function makeCols(colsNum) {
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < colsNum; j++) {
             let newCell = document.createElement("div");
+            newCell.id = "cell";
             rows[j].appendChild(newCell).className = "cell"
         }
     }
 }
 
 makeGrid();
+
+let cell = document.querySelectorAll(".cell");
+
+for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener("mouseover", () => {
+        cell[i].classList.add("cell-hover");
+    });
+    cell[i].addEventListener("mouseout", () => {
+        cell[i].classList.remove("cell-hover");
+    })
+}
+
+function hoverAdd() {
+    this.classList.add("cell-hover");
+}
+
+function hoverRemove() {
+    this.classList.remove("cell-hover");
+}
+
+cell.addEventListener("mouseover", hoverAdd);
+//cell.addEventListener("mouseout", hoverRemove);
